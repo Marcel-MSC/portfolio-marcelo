fd55173d8d3098658cd769784f37a401362d4507
Marcel-MSC <marcelomarcos.s.c@gmail.com>
2022-02-21 16:33:26 -0300

    change URL to my gitconnected profile

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [user, setUser] = useState(null);
   useEffect(() => {
     // NOTE: Use your username below
-    fetch('https://gitconnected.com/v1/portfolio/treyhuffine')
+    fetch('https://gitconnected.com/v1/portfolio/marcel-msc')
       .then(res => res.json())
       .then(user => {
         setUser(user);
