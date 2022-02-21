98d2dee0fe01fb726d8a04323119933a4df3909e
Marcel-MSC <marcelomarcos.s.c@gmail.com>
2022-02-21 16:11:48 -0300

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Me from './Me';
+import Projects from './Projects';
+import Work from './Work';
+import Education from './Education';
+
+const Pages = ({ user }) => {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Me user={user} />} />
+        <Route path="/projects" element={<Projects user={user} />} />
+        <Route path="/work" element={<Work user={user} />} />
+        <Route path="/education" element={<Education user={user} />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default Pages;
