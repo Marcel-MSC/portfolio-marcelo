41042715a6761e281de20902f06a8c34d5822338
Marcel-MSC <marcelomarcos.s.c@gmail.com>
2024-05-29 20:27:08 -0300

    comment import of openstreetmap on index of pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Me from './Me';
 import Projects from './Projects';
 import Work from './Work';
 import Education from './Education';
-import OpenStreetMap from './OpenStreetMap';
+// import OpenStreetMap from './OpenStreetMap';
 
 const Pages = ({ user }) => {
   return (
